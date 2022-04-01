fd15ed17467153ccc9056f2bd2c5b3875ebd0267
Wale Ayandiran (walecloud) <walecloud@gmail.com>
2022-04-01 02:18:07 +0100

    create new anchor component, open new tab

---
+++ src/components/AnchorLink.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AnchorLink(props) {
+  const { url, newTab } = props;
+  return (
+    <a href={url} rel="noopener noreferrer" target={newTab && "_blank"}>
+      {props.children}
+    </a>
+  )
+}
